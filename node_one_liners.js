c2e104b88f27e6639b5a4eadd7b4958f38b9dfd6
Norbert Budincsevity <nbudincsevity@gmail.com>
2019-05-17 15:16:26 +0200

    Add sleep one liner

--- node_one_liners.js
+++ node_one_liners.js
@@ -3,3 +3,10 @@ node -p "os.cpus().length"
 
 // Supported V8 options
 node --v8-options | less
+
+// Sleep for 35sec
+function sleep(millis) {
+  return new Promise(resolve => setTimeout(resolve, millis));
+}
+
+await sleep(35000);
